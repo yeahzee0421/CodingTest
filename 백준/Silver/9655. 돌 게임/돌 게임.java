376c02c3dba198b3d56b9a20abeb537a4d317dd4
YeJi Lee <107865510+yeahzee0421@users.noreply.github.com>
2024-03-26 21:15:22 +0900

    [Silver V] Title: 돌 게임, Time: 128 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println((n % 2 == 1) ? "SK" : "CY");
+
+	}
+
+}
