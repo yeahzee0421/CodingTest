6270c5ad199663feeb3a182b5984e5bbfadb8ba4
YeJi Lee <107865510+yeahzee0421@users.noreply.github.com>
2024-04-07 14:16:18 +0900

    [Silver III] Title: 눈덩이 굴리기, Time: 132 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.java
+++ 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.java
@@ -1,40 +1,36 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-
-    static int[] snow = new int[101]; 
-    static int N, M;
-    static int answer = 0; 
-
-    public static void func(int time, int size, int pos) {
-        if (time == M || pos >= N) { 
-            answer = Math.max(size, answer);
-            return;
-        }
-
-        // 눈덩이를 굴리는 경우, 던지는 경우
-        for (int i = 1; i <= 2; i++) {
-            if (i == 1) {
-                func(time + 1, size + snow[pos + i], pos + i); 
-            } else {
-                func(time + 1, (size / 2) + snow[pos + i], pos + i); 
-            }
-        }
+public class Main{
+    static int n, m, answer;
+    static int a[];
+    public static void backtracking(int size, int pos, int time) {
+    	if(pos == n || time == m) {
+    		answer = Math.max(answer, size);
+    		return;
+    	}
+    	for(int i = 1; i <= 2; i++) {
+    		if(i==1) backtracking(size+a[pos+i], pos+i, time+1);
+    		else backtracking((size/2)+a[pos+i], pos+i, time+1);
+    	}
     }
+    
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-        M = sc.nextInt();
-
-        for (int i = 1; i <= N; i++) {
-            snow[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        answer = 0;
+        a = new int[101];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++) {
+        	a[i] = Integer.parseInt(st.nextToken());
         }
-
-        func(0, 1, 0);
+        backtracking(1, 0, 0);
         System.out.println(answer);
-
-        sc.close();
     }
+
 }
